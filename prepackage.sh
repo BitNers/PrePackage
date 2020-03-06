9fee84b7c602e1ded289f200afd217e5b2e55ddd
BitNers <wesleyalvessilva@outlook.com.br>
2020-03-05 22:59:28 -0300

    Alteracoes

--- prepackage.sh
+++ prepackage.sh
@@ -12,7 +12,8 @@ sudo apt-get install -y \
     python \
     python3 \
     preload \
-    htop
+    htop \
+    git
 
 #Mongo DB Public key
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
@@ -26,11 +27,8 @@ sudo apt-key fingerprint	 0EBFCD88
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu"
 
-#Atom GitHub
-sudo add-apt-repository ppa:webupd8team/atom
-
-#Flat Remix Gnome GitHub
-sudo add-apt-repository ppa:daniruiz/flat-remix
+#Telegram Public Key
+sudo add-apt-repository ppa:atareao/telegram
 
 #==============instalação============#
 sudo apt-get update
@@ -39,36 +37,20 @@ sudo apt install -y windscribe-cli
 
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-sudo apt install -y atom
-sudo snap install atom
-
 sudo apt install -y r-base
 
 sudo apt install -y mongodb-org
 
 sudo apt install -y default-jre
 sudo apt install -y default-jdk
-sudo snap install --classic eclipse
+sudo snap install -y --classic eclipse
 
 sudo apt install -y lm-sensors
 sudo apt install -y zsh
 
-#instalação do FlatRemixGnome
-sudo apt install -y flat-remix-gnome
-git clone https://github.com/daniruiz/flat-remix-gnome
-mv flat-remix-gnome ~/.themes
+sudo apt install -y gnome-tweak-tool
 
+sudo apt install -y telegram
+sudo snap install -y telegram-desktop
 
-#instalação do Xampp
-mkdir ~/ApacheInstaller
-cd ~/ApacheInstaller
-wget https://www.apachefriends.org/xampp-files/7.2.2/xampp-linux-x64-7.2.2-0-installer.run
-sudo chmod +x xampp-linux-x64-7.2.2-0-installer.run
-
-sudo ./xampp-linux-x64-7.2.2-0-installer.run
-sudo apt upgrade
-
-# Chaves públicas: MongoDB, Windscribe, Docker
-# Instalar Default-JRE
-# Eclipse, MongoDB, Atom, Xampp, Windscribe, Python Python3, R-Core
-#Tmux, Flat-remix, htop, 
+sudo apt install -y apache2
